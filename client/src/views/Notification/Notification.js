5c309da972843319fa1c37657015658f5ce7f577
Oumar Diarra <oumardiarra@Oumars-iMac.local>
2019-05-21 15:40:04 -0500

    changes

--- client/src/views/Notification/Notification.js
+++ client/src/views/Notification/Notification.js
@@ -15,11 +15,26 @@ const Notification = ({notification, onClick}) => {
     return (
          <tbody> 
             <tr> 
-                <td><h2 style={styles.h2Style} className=" col-5">{notification.title}</h2></td>
-                <td> <p className = "f2 ">{notification.content} </p> </td>
-                <td> <p ><Link className = "white i" to={'/customers/' + notification.cu}><i className="fa fa-eye" /></Link></p></td>
                 <td>
-                    <p style={styles.pointer}  onClick = {onClick}> <i className="fa fa-archive" /></p>
+                    <h2 style={styles.h2Style} className=" col-5">
+                        {notification.title}
+                    </h2>
+                </td>
+                <td>
+                 <p className = "f2 ">{notification.content} </p>
+                </td>
+                <td><p>
+                    <Link 
+                        className = "white i" to={'/customers/' + notification.cu}>
+                        <i className="fa fa-eye" />
+                    </Link>
+                    </p></td>
+                <td>
+                    <p 
+                        style={styles.pointer}  
+                        onClick = {onClick}> 
+                        <i className="fa fa-archive" />
+                    </p>
                 </td>
             </tr>
          </tbody>
